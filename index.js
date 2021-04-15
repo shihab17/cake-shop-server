06d3236e249247e71f004603558d743989d2c7e5
shihab17 <shihabahmed620@gmail.com>
2021-04-15 15:38:28 +0600

    first commit

---
+++ index.js
@@ -0,0 +1,14 @@
+const express = require('express');
+const app = express();
+const cors = require('cors');
+const bodyParser = require('body-parser');
+require('dotenv').config;
+const port = process.env.PORT || 5000;
+app.use(cors());
+app.use(bodyParser.json());
+app.get('/',(req,res) => {
+    res.send('Welcome to Cake-shop Server')
+})
+app.listen(port , () => {
+    console.log(`Example app listening at http://localhost:${port}`)
+})
